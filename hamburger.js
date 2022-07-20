fb66e1110577226939064e6533e4972dcfa8d105
Luke <stott.garrett@gmail.com>
2022-07-20 16:52:23 -0500

    Updated Website

---
+++ hamburger.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.nav-toggle');
+const navsub = document.querySelector('.mobile-menu');
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open')
+    navsub.classList.toggle('collapse')})
---
+++ main.js
@@ -0,0 +1,18 @@
+var $window   = $(window),
+    navheight = $('.heading').height();
+    menuheight = $('.heading #fixed-nav #fixed-line').height();
+
+function sticky(){
+  var calcScroll = ((navheight - menuheight) / 2);
+  var scrollTop = $window.scrollTop();
+  if (scrollTop > calcScroll ) {
+    $('#fixed-line').addClass('sticky');
+    $('#fixed-nav').addClass('sticky');
+  } else {
+    $('#fixed-line').removeClass('sticky');
+    $('#fixed-nav').removeClass('sticky');
+  }
+} 
+
+$window.on('scroll', sticky); 
+
