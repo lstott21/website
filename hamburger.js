a1311045be0e8f24b6a90b56c4f0d0d7ca36e013
Luke <stott.garrett@gmail.com>
2022-07-23 21:24:48 -0500

    Updated Website

--- hamburger.js
+++ hamburger.js
@@ -5,3 +5,4 @@ const navsub = document.querySelector('.mobile-menu');
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('open')
     navsub.classList.toggle('collapse')})
+
--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-var $window   = $(window),
-    navheight = $('.heading').height();
-    menuheight = $('.heading #fixed-nav #fixed-line').height();
+// var $window   = $(window),
+//     navheight = $('.heading').height();
+//     menuheight = $('.heading #fixed-nav #fixed-line').height();
 
-function sticky(){
-  var calcScroll = ((navheight - menuheight) / 2);
-  var scrollTop = $window.scrollTop();
-  if (scrollTop > calcScroll ) {
-    $('#fixed-line').addClass('sticky');
-    $('#fixed-nav').addClass('sticky');
-  } else {
-    $('#fixed-line').removeClass('sticky');
-    $('#fixed-nav').removeClass('sticky');
-  }
-} 
+// function sticky(){
+//   var calcScroll = ((navheight - menuheight) / 2);
+//   var scrollTop = $window.scrollTop();
+//   if (scrollTop > calcScroll ) {
+//     $('#fixed-line').addClass('sticky');
+//     $('#fixed-nav').addClass('sticky');
+//   } else {
+//     $('#fixed-line').removeClass('sticky');
+//     $('#fixed-nav').removeClass('sticky');
+//   }
+// } 
 
-$window.on('scroll', sticky); 
+// $window.on('scroll', sticky); 
 
